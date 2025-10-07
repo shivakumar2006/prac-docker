16fb7e352688988902af3fe16cb7a96d401f0d9e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-07 23:01:19 +0530

    comit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import Home from "./Components/home";
+import "./App.css"
+
+const App = () => {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
+
+export default App; 
---
+++ frontend/src/Components/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const home = () => {
+  return (
+    <div className='bg-black w-full text-white'>home</div>
+  )
+}
+
+export default home;
